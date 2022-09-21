d5f38df0ff18874aa62f50f5223ba8fbcf9cfb42
Vasko <vaskotanchev@yahoo.com>
2022-09-21 16:16:26 +0200

    widgets component complited

---
+++ src/homepage/components/widgets/Widgets.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+
+import { widgetData } from "./components/WidgetData";
+
+import "./Widgets.css";
+
+const Widgets = () => {
+  return (
+    <section className="widget-component">
+      <div className="background-overlay"></div>
+      <div className="el-shape shape-top" data-negative="false">
+        <svg viewBox="0 0 1000 100" preserveAspectRatio="none">
+          <path className="shape-fill" d="M0,6V0h1000v100L0,6z"></path>
+        </svg>
+      </div>
+      <div className="widgets-container">
+        <div className="widgets-title">
+          Houzez offers a wide range of drag-and-drop widgets to assist you in
+          designing your pages
+        </div>
+        <div className="widgets-items">
+          <div className="widget-cards">
+            {widgetData.map((item, index) => (
+              <div key={index} className="widget-item">
+                <div className="widget-image">
+                  <img src={item.img} alt={item.index} />
+                </div>
+                {item.title}
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+      <div className="el-shape shape-bottom" data-negative="false">
+        <svg viewBox="0 0 1000 100" preserveAspectRatio="none">
+          <path className="shape-fill" d="M0,6V0h1000v100L0,6z"></path>
+        </svg>
+      </div>
+    </section>
+  );
+};
+
+export default Widgets;
---
+++ src/homepage/components/widgets/components/WidgetData.jsx
@@ -0,0 +1,23 @@
+import helpHouse from "../../../../assets/widget-help-house.png";
+import house from "../../../../assets/widget-house.png";
+import realEstate from "../../../../assets/widget-real-estate.png";
+import rent from "../../../../assets/widget-rent.png";
+
+export const widgetData = [
+  {
+    img: helpHouse,
+    title: "LOOKING TO BUY",
+  },
+  {
+    img: house,
+    title: "SELL YOUR HOME",
+  },
+  {
+    img: realEstate,
+    title: "RENT A PLACE",
+  },
+  {
+    img: rent,
+    title: "NEED HELP?",
+  },
+];
