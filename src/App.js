afd911fef71f9a0cce093fe9d1e0fb3f6c0e0c98
Vasko <vaskotanchev@yahoo.com>
2022-10-02 23:58:32 +0200

    scroll button to top completed

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import AboutUs from "./About Us/AboutUs";
-import ContactUs from "./contactUs/ContactUs";
-import Footer from "./Footer/Footer";
+// import ScrollToTop from "react-scroll-to-top";
 
 import Header from "./header/Header";
 import HomePage from "./homepage/HomePage";
+import AboutUs from "./About Us/AboutUs";
+import ContactUs from "./contactUs/ContactUs";
+import Footer from "./Footer/Footer";
+import ScrollToTop from "./ScrollToTop";
 
 function App() {
   return (
     <div className="App">
+      <ScrollToTop />
       <Router>
         <Header />
         <Routes>
